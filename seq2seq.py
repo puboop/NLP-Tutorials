27e2b17f8ddb90efb116f47931528b77eb61353c
desk-white <desk-white@163.com>
2024-07-22 22:38:10 +0800

    seq2seq bug修复

--- seq2seq.py
+++ seq2seq.py
@@ -34,7 +34,8 @@ class Seq2Seq(keras.Model):
         # predict decoder
         self.decoder_eval = tfa.seq2seq.BasicDecoder(
             cell=self.decoder_cell,
-            sampler=tfa.seq2seq.sampler.GreedyEmbeddingSampler(),       # sampler for predict
+            # sampler=tfa.seq2seq.sampler.GreedyEmbeddingSampler(),       # sampler for predict
+            sampler=tfa.seq2seq.sampler.GreedyEmbeddingSampler(self.dec_embeddings),       # sampler for predict
             output_layer=decoder_dense
         )
 
